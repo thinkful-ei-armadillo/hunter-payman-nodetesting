9c2eddcb049811449fab455c28b7fc524255173f
Payman Kossari <Paymankossari@gmail.com>
2019-02-20 14:54:01 -0500

    frequency drill

--- express_testing/test/app.test.js
+++ express_testing/test/app.test.js
@@ -17,11 +17,63 @@ describe('GET /sum', () => {
       .query({ a: 8, b: 4 })
       .expect(200, '8 divided by 4 is 2');
   });
+
+  it('should return 400 if a is missing', () => {
+    return request(app)
+      .get('/sum')
+      .query({ b: 4 })
+      .expect(400, 'Value for a is needed');
+  });
 });
 
-it('should return 400 if a is missing', () => {
-  return request(app)
-    .get('/sum')
-    .query({ b: 4 })
-    .expect(400, 'Value for a is needed');
+describe('Frequnecy Endpoint', () => {
+  it('Query passed in return 200 when query exists', () => {
+    return request(app)
+      .get('/frequency')
+      .query({ s: 'hello' })
+      .expect(200);
+  });
+  it('return 400 when query does not exist', () => {
+    return request(app)
+      .get('/frequency')
+      .expect(400);
+  });
+
+  it('return 200 if only spaces', () => {
+    return request(app)
+      .get('/frequency')
+      .query({ s: '    ' })
+      .expect(200);
+  });
+
+  it('response should be an object with the correct keys', () => {
+    const expected = {
+      unique: 4,
+      average: 1,
+      highest: 2,
+      h: 1,
+      e: 1,
+      l: 2,
+      o: 1
+    };
+    return request(app)
+      .get('/frequency')
+      .query({ s: 'hello' })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.any.keys('unique', 'average', 'highest');
+        expect(res.body).to.have.all.keys(
+          'average',
+          'highest',
+          'unique',
+          'h',
+          'e',
+          'l',
+          'o'
+        );
+      });
+    expect(res.body).to.eql(expected);
+  });
 });
