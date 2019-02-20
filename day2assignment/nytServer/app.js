988b7676f69b4590bff02fb249fc45963801457b
Payman Kossari <Paymankossari@gmail.com>
2019-02-20 14:56:22 -0500

    development dependencies

---
+++ day2assignment/nytServer/app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const books = require('./books');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+
+app.get('/books', (req, res) => {
+  const { search = '', sort } = req.query;
+
+  if(sort) {
+    if(!['title', 'rank'].includes(sort)) {
+      return res
+        .status(400)
+        .send('Sort must be one of title or rank');
+    }
+  }
+
+  let results = books
+    .filter(book =>
+      book
+        .title
+        .toLowerCase()
+        .includes(search.toLowerCase()));
+
+  if(sort) {
+    results
+      .sort((a, b) => {
+        return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+      });
+  }
+
+  res
+    .json(results);
+});
+
+app.listen(8000, (req, res) => {
+  console.log('Listening on port 8000');
+});
