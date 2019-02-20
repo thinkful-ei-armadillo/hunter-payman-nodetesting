61cf28973d506c99adf16026f475d535319d0d08
Payman Kossari <Paymankossari@gmail.com>
2019-02-20 15:05:00 -0500

    followed along with nytServer in curriculum

--- day2assignment/nytServer/app.js
+++ day2assignment/nytServer/app.js
@@ -13,32 +13,23 @@ app.use(morgan('dev'));
 app.get('/books', (req, res) => {
   const { search = '', sort } = req.query;
 
-  if(sort) {
-    if(!['title', 'rank'].includes(sort)) {
-      return res
-        .status(400)
-        .send('Sort must be one of title or rank');
+  if (sort) {
+    if (!['title', 'rank'].includes(sort)) {
+      return res.status(400).send('Sort must be one of title or rank');
     }
   }
 
-  let results = books
-    .filter(book =>
-      book
-        .title
-        .toLowerCase()
-        .includes(search.toLowerCase()));
+  let results = books.filter(book =>
+    book.title.toLowerCase().includes(search.toLowerCase())
+  );
 
-  if(sort) {
-    results
-      .sort((a, b) => {
-        return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
-      });
+  if (sort) {
+    results.sort((a, b) => {
+      return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+    });
   }
 
-  res
-    .json(results);
+  res.json(results);
 });
 
-app.listen(8000, (req, res) => {
-  console.log('Listening on port 8000');
-});
+module.exports = app;
---
+++ day2assignment/nytServer/server.js
@@ -0,0 +1,5 @@
+const app = require('./app.js');
+
+app.listen(8000, () => {
+  console.log('Server started on PORT 8000');
+});
