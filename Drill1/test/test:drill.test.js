9af694c68b30c03a9981653b04d0934a78cff687
Payman Kossari <Paymankossari@gmail.com>
2019-02-20 12:33:19 -0500

    added some more tests to first drill

--- Drill1/test/test:drill.test.js
+++ Drill1/test/test:drill.test.js
@@ -11,13 +11,29 @@ describe('sort function', () => {
     const actualAnswer = newList[1];
     expect(actualAnswer).to.equal(expectedAnswer);
   });
-   
+
   it('should contain numbers in array', () => {
     let list = [2, 9, 10, 4, 7];
     let newList = sort(list);
-    const expectedAnswer = 'number';
-    const actualAnswer = typeof newList[1];
-    expect(actualAnswer).to.equal(expectedAnswer);
+    expect(newList[1]).to.be.a('number');
+  });
+
+  it('should be an array', () => {
+    let list = [2, 9, 10, 4, 7];
+    let newList = sort(list);
+    expect(newList).to.be.an('array');
+  });
+
+  it('should work on an empty array', () => {
+    let list = [];
+    let newList = sort(list);
+    expect(newList).to.deep.equal([]);
+  });
+
+  it('test on non array', () => {
+    let list = { hi: 'jooce', sup: 'bark' };
+    let newList = sort(list);
+    expect(newList).to.not.be.an('array');
   });
 
   it('should sort numbers in array', () => {
@@ -27,7 +43,7 @@ describe('sort function', () => {
     let newList2 = sort(list2);
     expect(newList1).to.deep.equal(newList2);
   });
-  
+
   it('should sort letters in array in ascending order', () => {
     let list = ['d', 'x', 'r', 'n', 'p'];
     let newList = sort(list);
@@ -44,11 +60,11 @@ describe('sort function', () => {
     expect(actualAnswer).to.equal(expectedAnswer);
   });
 
-  it('sorts both numbers and letters together', () => {
-    let list = [-3, 'a', 5, 'e', -1, 9, 'i' , 'o', 'u'];
-    let list2 = [-3, 5, -1, 9, 'a', 'i' , 'e', 'o', 'u'];
+  /*it('sorts both numbers and letters together', () => {
+    let list = [-3, 'a', 5, 'e', -1, 9, 'i', 'o', 'u'];
+    let list2 = [-3, 5, -1, 9, 'a', 'i', 'e', 'o', 'u'];
     let newList1 = sort(list);
     let newList2 = sort(list2);
     expect(newList1).to.deep.equal(newList2);
-  });
+  });*/
 });
